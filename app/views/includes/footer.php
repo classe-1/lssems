1ca1276ddd8133204c009f74e93fb9a6cce949c0
Iliass Raihani <i.raihani@outlook.com>
2020-07-08 04:27:44 +0100

    bootstrap

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -1,5 +1,10 @@
     <footer>
       © Copyright 2020-2021. All rights reserved. Developed by Omega.
     </footer>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/bootstrap/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="assets/bootstrap/js/popper-1.12.3.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap-4.0.0-beta.2.min.js"></script>
+    <script src="assets/js/script.js"></script>
   </body>
 </html>
--- app/views/includes/header.php
+++ app/views/includes/header.php
@@ -5,7 +5,28 @@
     <?php include_once 'views/includes/head.php' ?>
     <title><?= ucfirst($page); ?> - Bricole</title>
 
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-4.0.0-beta.2.min.css">
 </head>
 <body class="bg-info">
     <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="index.php?page=home">Bricole</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php?page=home">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?page=categories">Categories</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php?page=contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </header>
